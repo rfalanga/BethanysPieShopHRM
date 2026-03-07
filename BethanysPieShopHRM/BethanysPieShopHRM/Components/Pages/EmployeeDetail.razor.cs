896d3c08a73537f3fd61167770a22352ac3cc06e
Rod Falanga <rfalanga@users.noreply.github.com>
2026-03-07 07:05:21 -0700

    Beginning to add EmployeeDetail

---
+++ BethanysPieShopHRM/BethanysPieShopHRM/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override void OnInitialized()
+        {
+            Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
+        }
+    }
+}
