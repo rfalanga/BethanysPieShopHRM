3ff7d7e49119d0d46459741b59fde908b289e81c
Rod Falanga <rfalanga@users.noreply.github.com>
2026-04-01 18:26:56 -0600

    beginning to add the EmployeeCard

---
+++ BethanysPieShopHRM/BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -0,0 +1,9 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Components
+{
+    public partial class EmployeeCard
+    {
+        public Employee Employee { get; set; } = default!;
+    }
+}
