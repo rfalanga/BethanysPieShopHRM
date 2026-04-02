adfe3b42d3f3ab3e366c2d017908bec7024a0cde
Rod Falanga <rfalanga@users.noreply.github.com>
2026-04-01 18:38:35 -0600

    Added the EmployeeCard component to the
    EmployeeOverview page.

--- BethanysPieShopHRM/BethanysPieShopHRM/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Components/EmployeeCard.razor.cs
@@ -1,9 +1,11 @@
 ﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieShopHRM.Components
 {
     public partial class EmployeeCard
     {
+        [Parameter]
         public Employee Employee { get; set; } = default!;
     }
 }
