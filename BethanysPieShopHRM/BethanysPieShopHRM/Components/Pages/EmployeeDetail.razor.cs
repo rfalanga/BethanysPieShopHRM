a0f89725c82c5087fb2f09ea3dce4ae69962025e
Rod Falanga <rfalanga@users.noreply.github.com>
2026-04-25 07:23:59 -0600

    This is so screwed up that I am going to start this section over again.
    I'm going to save what I've got so far, because I don't want to type
    everything over again.

--- BethanysPieShopHRM/BethanysPieShopHRM/Components/Pages/EmployeeDetail.razor.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Components/Pages/EmployeeDetail.razor.cs
@@ -3,6 +3,7 @@ using BethanysPieShopHRM.Data;
 using BethanysPieShopHRM.Services;
 using BethanysPieShopHRM.Shared.Domain;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 namespace BethanysPieShopHRM.Components.Pages
 {
@@ -30,6 +31,17 @@ namespace BethanysPieShopHRM.Components.Pages
         private void ChangeHolidayState()
         {
             Employee.IsOnHoliday = !Employee.IsOnHoliday;
+        }
+
+        public async ValueTask<ItemsProviderRequest<TimeRegistration>> LoadTimeRegistrations(ItemsProviderRequest itemsProviderRequest)
+        { 
+            int totalNumberOfTimeRegistrations = await TimeRegistrationDataService.GetTimeRegistrationCountForEmployeeIdAsync(EmployeeId);
+
+            var numberOfTimeRegistrations = Math.Min(itemsProviderRequest.Count, totalNumberOfTimeRegistrations - itemsProviderRequest.StartIndex);
+
+            var listItems = await TimeRegistrationDataService.GetTimeRegistrationsForEmployeeIdAsync(EmployeeId, itemsProviderRequest.StartIndex, numberOfTimeRegistrations);
+            
+            return new ItemsProviderResult<TimeRegistration>(listItems, totalNumberOfTimeRegistrations);
         }
     }
 }
