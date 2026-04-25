6fe2fa517d092cc01a251cbf53da07502de54163
Rod Falanga <rfalanga@users.noreply.github.com>
2026-04-24 18:24:45 -0600

    And this is what Gill came up with, or at least what's in the
    "end" folder. Which might be what comes up later.

--- BethanysPieShopHRM/BethanysPieShopHRM/Contracts/Repositories/ITimeRegistrationRepository.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Contracts/Repositories/ITimeRegistrationRepository.cs
@@ -5,7 +5,7 @@ namespace BethanysPieShopHRM.Contracts.Repositories
     public interface ITimeRegistrationRepository
     {
         Task<List<TimeRegistration>> GetTimeRegistrationsForEmployeeAsync(int employeeId);
-        Task<List<TimeRegistration>> GetPagedTimeRegistrationsForEmployeeAsync(int employeeId, DateTime startDate, DateTime endDate);
+        Task<List<TimeRegistration>> GetPagedTimeRegistrationsForEmployeeAsync(int employeeId, int pageSize, int start);
         Task<int> GetTimeRegistrationCountForEmployeeIdAsync(int employeeId);
     }
 }
