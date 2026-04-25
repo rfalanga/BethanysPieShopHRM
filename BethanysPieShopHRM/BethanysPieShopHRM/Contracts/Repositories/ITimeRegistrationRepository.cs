786e223b8904f4eb39eaae139677264e1b765604
Rod Falanga <rfalanga@users.noreply.github.com>
2026-04-24 18:23:29 -0600

    This is what GitHub Copilot came up with
    the thing that's hard to tell is for the
    GetPagedTimeRegistrationsForEmployeeAsync method. Gill didn't
    scroll to the right, so I don't know if startDate and endDate are
    correct. It's what GitHub Copilot came up with.

--- BethanysPieShopHRM/BethanysPieShopHRM/Contracts/Repositories/ITimeRegistrationRepository.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Contracts/Repositories/ITimeRegistrationRepository.cs
@@ -5,5 +5,7 @@ namespace BethanysPieShopHRM.Contracts.Repositories
     public interface ITimeRegistrationRepository
     {
         Task<List<TimeRegistration>> GetTimeRegistrationsForEmployeeAsync(int employeeId);
+        Task<List<TimeRegistration>> GetPagedTimeRegistrationsForEmployeeAsync(int employeeId, DateTime startDate, DateTime endDate);
+        Task<int> GetTimeRegistrationCountForEmployeeIdAsync(int employeeId);
     }
 }
