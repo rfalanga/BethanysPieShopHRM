9e381fca505137db49ee637dbdac5a2e5ea35317
Rod Falanga <rfalanga@users.noreply.github.com>
2026-05-25 08:44:04 -0600

    I believe I've made all the changes that Gill has done
    but am getting an error in the GitHub Copilot.

---
+++ BethanysPieShopHRM/BethanysPieShopHRM.Client/Services/ClientEmployeeDataService.cs
@@ -0,0 +1,44 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using System.Text.Json;
+
+namespace BethanysPieShopHRM.Client.Services
+{
+    public class ClientEmployeeDataService : IEmployeeDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClientEmployeeDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<Employee> AddEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployeeAsync(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+        {
+            var list = await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>(
+                await _httpClient.GetStreamAsync($"api/employee"),
+                new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }
+            );
+            return list;
+        }
+
+        public Task<Employee> GetEmployeeDetailsByIdAsync(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
