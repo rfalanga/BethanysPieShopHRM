03f3213161c8894a0378be9ea0f3449f8701bf5a
Rod Falanga <rfalanga@users.noreply.github.com>
2026-06-03 19:16:23 -0600

    Stopping for tonight. Note: this is incomplete!

--- BethanysPieShopHRM/BethanysPieShopHRM/Components/Map.razor.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Components/Map.razor.cs
@@ -1,5 +1,6 @@
 ﻿using BethanysPieShopHRM.Shared.Model;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BethanysPieShopHRM.Components
 {
@@ -12,5 +13,16 @@ namespace BethanysPieShopHRM.Components
 
         [Parameter]
         public List<Marker> Markers { get; set; } = new List<Marker>(); // Initialize to an empty list to avoid null reference issues; suggested by GitHub Copilot
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)    // Note: Gill do NOT have this conditional! He only had the await below, so I'm not sure this is correct.
+            {
+                await JSRuntime.InvokeVoidAsync("deliveryMap.showOrUpdate", elementId, Markers);    // deliveryMap is the name of the JavaScript object, and showOrUpdate is the name of the function in that object. elementId and Markers are the parameters being passed to that function.
+            }
+        }
     }
 }
