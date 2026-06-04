574f1fea4a5bd66cbfb92e575364f160609409a1
Rod Falanga <rfalanga@users.noreply.github.com>
2026-06-03 19:01:02 -0600

    Added code to the Map code behind file

--- BethanysPieShopHRM/BethanysPieShopHRM/Components/Map.razor.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Components/Map.razor.cs
@@ -1,6 +1,16 @@
-﻿namespace BethanysPieShopHRM.Components
+﻿using BethanysPieShopHRM.Shared.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components
 {
     public partial class Map
     {
+        string elementId = $"map-{Guid.NewGuid():D}";
+
+        [Parameter]
+        public double Zoom { get; set; }
+
+        [Parameter]
+        public List<Marker> Markers { get; set; } = new List<Marker>(); // Initialize to an empty list to avoid null reference issues; suggested by GitHub Copilot
     }
 }
